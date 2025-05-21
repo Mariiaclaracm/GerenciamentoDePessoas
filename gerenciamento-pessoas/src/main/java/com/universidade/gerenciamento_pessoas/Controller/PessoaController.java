8b95752e51114e9bb9c212a1fcebd75b64bee4c3
mariiaclaracm <mariiamendes1006@gmail.com>
2025-05-21 11:02:46 -0300

    Atualizado Oficialmente

--- gerenciamento-pessoas/src/main/java/com/universidade/gerenciamento_pessoas/Controller/PessoaController.java
+++ gerenciamento-pessoas/src/main/java/com/universidade/gerenciamento_pessoas/Controller/PessoaController.java
@@ -2,10 +2,12 @@ package com.universidade.gerenciamento_pessoas.Controller;
 
 import com.universidade.gerenciamento_pessoas.DTO.PessoaDTO;
 import com.universidade.gerenciamento_pessoas.model.Pessoa;
+import com.universidade.gerenciamento_pessoas.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.List;
 
 @RestController
@@ -13,15 +15,40 @@ import java.util.List;
 public class PessoaController {
 
     @Autowired
+    private PessoaService pessoaService;
 
     @PostMapping
     public ResponseEntity<Pessoa> criarPessoa(@RequestBody PessoaDTO pessoaDTO) {
+        Pessoa pessoaSalva = pessoaService.criarPessoa(pessoaDTO);
         return ResponseEntity.status(201).body(pessoaSalva);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Pessoa> getPessoaPorId(@PathVariable Long id) {
+        Optional<Pessoa> pessoa = pessoaService.buscarPorId(id);
         return pessoa.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Pessoa> atualizarPessoa(@PathVariable Long id, @RequestBody PessoaDTO pessoaDTO) {
+        Pessoa pessoaAtualizada = pessoaService.atualizarPessoa(id, pessoaDTO);
+        return ResponseEntity.ok(pessoaAtualizada);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPessoa(@PathVariable Long id) {
+        pessoaService.deletarPessoa(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // GET com filtro por nome e idade
+    @GetMapping("/buscar")
+    public ResponseEntity<List<Pessoa>> buscarPorNomeEIdade(
+            @RequestParam String nome,
+            @RequestParam int idade
+    ) {
+        List<Pessoa> pessoas = pessoaService.buscarPorNomeEIdade(nome, idade);
+        return ResponseEntity.ok(pessoas);
+    }
+}
